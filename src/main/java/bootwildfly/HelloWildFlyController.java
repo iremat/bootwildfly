c97042a7eb3fc0d84dd1bab56b31cb592284bff7
imatysiak <iremat@wp.pl>
2018-10-05 14:30:54 +0200

    add logging

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,16 +3,26 @@ package bootwildfly;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+
 @RestController
 public class HelloWildFlyController {
+    Logger logger = LogManager.getLogger(HelloWildFlyController.class);
+
+
     @RequestMapping("hello")
     public String sayHello() {
+        logger.debug("sayHello");
         return
                 String.format("{\"hello_ms\": %d}", System.currentTimeMillis());
     }
 
     @RequestMapping("getmessages")
     public String readFromKafka() {
+        logger.debug("getmessages");
+
         return
                 String.format("{\"messages\": %s}", "N/A");
     }
